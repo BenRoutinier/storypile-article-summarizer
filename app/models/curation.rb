50d9a1198ec3a558417e67f09c33addab027a8fc
Jessica Fernsler <jessie.fernsler@gmail.com>
2025-12-09 13:48:33 +0000

    user stories curations and bookmarks

--- app/models/curation.rb
+++ app/models/curation.rb
@@ -1,5 +1,6 @@
 class Curation < ApplicationRecord
   has_many :bookmarks, dependent: :destroy
+  has_many :articles, through: :bookmarks
   belongs_to :user
 
   validates :title, presence: true, uniqueness: { scope: :user_id }
