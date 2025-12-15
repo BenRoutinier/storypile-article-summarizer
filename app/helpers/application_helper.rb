947804cf47d1482484f11c2e7e40e8bae3228e73
Lalon Sander <lalon.sander@gmail.com>
2025-12-15 12:27:30 +0100

    refactor navbar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
   def current_section_label
-    return "My Articles" if current_page?(root_path)
+    return "My Articles" if current_page?(articles_path)
     return "My Summary Prompts" if current_page?(summary_prompts_path)
     return "My Curations" if current_page?(curations_path)
     return "Favourites" if current_page?(favourites_articles_path)
